64fa6ed126603959c1a9499735f67e29adb267ec
unknown <yh.choi@kornic.co.kr>
2019-08-14 13:17:44 +0900

    const qualifier https://wiki.sei.cmu.edu/confluence/display/c/STR05-C.+Use+pointers+to+const+when+referring+to+string+literals

--- rule7_4_1.c
+++ rule7_4_1.c
@@ -2,13 +2,13 @@
 
 void modify(char **s)
 {
-  char *new_name = "Australia";
+  const char *new_name = "Australia";
   *s = new_name;
 }
 
 int main()
 {
-  char *name = "New Holland";
+  const char *name = "New Holland";
   modify(&name);
   printf("%s\n", name);
   return 0;
